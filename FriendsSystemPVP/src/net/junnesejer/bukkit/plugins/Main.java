b080c37a2d09231ed4298ca117cfcd26040e23a7
Yuck Fou <junnesejer@gmail.com>
2013-01-05 18:12:03 +0100

    Committed new changes -> NOT UP TO DATE AND NOT COMPLETE!!!

--- FriendsSystemPVP/src/net/junnesejer/bukkit/plugins/Main.java
+++ FriendsSystemPVP/src/net/junnesejer/bukkit/plugins/Main.java
@@ -1,9 +1,15 @@
 package net.junnesejer.bukkit.plugins;
 
+import java.io.File;
+import java.util.logging.Logger;
+
+import net.junnesejer.bukkit.plugins.commands.FriendExecutor;
 import org.bukkit.plugin.java.JavaPlugin;
 
 //Please note I use the latest dev build 1.4.6-R0.4!
 public class Main extends JavaPlugin{
+	
+	public Logger log;
 	
 	@Override
 	public void onDisable(){
@@ -13,5 +19,11 @@ public class Main extends JavaPlugin{
 	@Override
 	public void onEnable(){
 		
+		//get data folder and check if it exists if not then create it
+		String pluginFolder = this.getDataFolder().getAbsolutePath();
+		(new File(pluginFolder)).mkdirs();
+		
+		
+		this.getCommand("friend").setExecutor(new FriendExecutor());
 	}
 }
